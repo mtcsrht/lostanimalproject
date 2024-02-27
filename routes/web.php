3842185762db602719a673e38ae070eb4d760902
Mate <>
2024-02-27 12:41:46 +0100

    feat: post page added, route added for /posts

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/posts', function(){
+    return view('posts');
+});
 require __DIR__.'/auth.php';
