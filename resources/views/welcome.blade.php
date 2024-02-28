15dcdcb8f29713261c41968f6acd0c09f969079e
Mate <>
2024-02-28 12:24:56 +0100

    ui: login, register, dashboard larger fonts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,12 +19,12 @@
                 @if (Route::has('login'))
                     <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                         @auth
-                            <a href="{{ url('/dashboard') }}" class="font-semibold text-cyan-600 xl:text-slate-50 hover:text-slate-300 dark:text-gray-100 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                            <a href="{{ url('/dashboard') }}" class="font-semibold text-cyan-600 xl:text-slate-50 hover:text-slate-300 dark:text-gray-100 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-cyan-500">Dashboard</a>
                         @else
-                            <a href="{{ route('login') }}" class="font-semibold text-cyan-600 xl:text-slate-50 hover:text-slate-300 dark:text-gray-100 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                            <a href="{{ route('login') }}" class="font-semibold text-xl text-cyan-600 xl:text-slate-50 hover:text-slate-300 dark:text-gray-100 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-cyan-500">Log in</a>
 
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-cyan-600 xl:text-slate-50  hover:text-slate-300 dark:text-gray-100 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-xl text-cyan-600 xl:text-slate-50  hover:text-slate-300 dark:text-gray-100 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-cyan-500">Register</a>
                             @endif
                         @endauth
                     </div>
