4aa8c3a3d94bf767f301dcc110d8451a0368231f
Máté Cserháti <mate.cserhati03@gmail.com>
2024-02-28 18:46:29 +0100

    update:cities api now queries for city name not postal code

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -11,8 +11,8 @@ class CityController extends Controller
     {
         return City::select(['irsz_id as postal_code', 'name as city', 'county_id as county'])->join('counties', 'cities.county_id', '=', 'counties.id')->select(['irsz_id as postal_code','counties.name as county', 'cities.name as city'])->get();
     }
-    public function show($id)
+    public function show($name)
     {
-        return City::where('irsz_id', 'like', $id . '%')->join('counties', 'cities.county_id', '=', 'counties.id')->select(['irsz_id as postal_code','counties.name as county', 'cities.name as city'])->get();
+        return City::where('cities.name', 'like', $name . '%')->join('counties', 'cities.county_id', '=', 'counties.id')->select(['irsz_id as postal_code','counties.name as county', 'cities.name as city'])->get();
     }
 }
