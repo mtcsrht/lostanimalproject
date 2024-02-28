2c6e0487951442d7bf7623f91acd3dc2b5c81dd4
Mate <>
2024-02-28 14:29:14 +0100

    fix: id names

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,29 +41,36 @@
         <!-- First name -->
         <div class="mt-4">
             <x-input-label for="firstname" :value="__('Keresztnév')"/>
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="firstname" placeholder="pl: János"/>
+            <x-text-input id="firstname" class="block mt-1 w-full" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="firstname" placeholder="pl: János"/>
             <x-input-error :messages="$errors->get('firstname')" class="mt-2" />
         </div>
 
         <!-- Last name -->
         <div class="mt-4">
             <x-input-label for="lastname" :value="__('Vezetéknév')"/>
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="lastname" :value="old('lastname')" required autofocus autocomplete="lastname" placeholder="pl: Nagy"/>
+            <x-text-input id="lastname" class="block mt-1 w-full" type="text" name="lastname" :value="old('lastname')" required autofocus autocomplete="lastname" placeholder="pl: Nagy"/>
             <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
         </div>
         <!-- Phone number -->
         <div class="mt-4">
             <x-input-label for="phonenumber" :value="__('Telefonszám')"/>
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="phonenumber" :value="old('phonenumber')" required autofocus autocomplete="phonenumber" placeholder="0630" />
+            <x-text-input id="phonenumber" class="block mt-1 w-full" type="text" name="phonenumber" :value="old('phonenumber')" required autofocus autocomplete="phonenumber" placeholder="0630" />
             <x-input-error :messages="$errors->get('phonenumber')" class="mt-2" />
         </div>
 
         <!-- Postal code -->
         <div class="mt-4">
-            <x-input-label for="postalcode" :value="__('Irányítószám')"/>
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="postalcode" :value="old('postalcode')" required autofocus autocomplete="postalcode"  placeholder="pl: 2660" />
-            <x-input-error :messages="$errors->get('postalcode')" class="mt-2" />
+            <x-input-label for="cityname" :value="__('Város')"/>
+            <x-text-input id="cityname" class="block mt-1 w-full" type="text" name="cityname" :value="old('cityname')" required autofocus autocomplete="cityname"  placeholder="pl: Vác" />
+            <x-input-error :messages="$errors->get('cityname')" class="mt-2" />
         </div>
+        <div class="mt-4">
+            <select multiple id="cities_multiple" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </select>
+        </div>
+        <script>
+           
+        </script>
         
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
