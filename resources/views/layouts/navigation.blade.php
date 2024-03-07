44f0c1e439b2cea3305f747c7afae3f4561a721a
Mate <>
2024-03-07 09:26:44 +0100

    feat: chatify

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Posts') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('chatify')" :active="request()->routeIs('chatify')">
+                        {{ __('Chat') }}
+                    </x-nav-link>
+                </div>
                 
             </div>
 
