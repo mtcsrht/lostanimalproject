079b6c01fe1984c17502b147fa0d9c3cc9fe87da
Máté Cserháti <mate.cserhati03@gmail.com>
2024-03-11 19:03:35 +0100

    ui:landing page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,7 @@
         <!-- Styles -->       
         @vite(['resources/css/app.css', 'resources/js/app.js'])     
     </head>
-    <body class="antialiased">
-        <div class="bg-lightmode dark:bg-darkmode bg-norepeat bg-cover aspect-square sm:aspect-auto">
+    <body class="antialiased bg-lightmode dark:bg-darkmode bg-norepeat bg-cover aspect-square sm:aspect-auto">
             <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center selection:bg-cyan-700 selection:text-white">
                 @if (Route::has('login'))
                     <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
@@ -29,17 +28,31 @@
                         @endauth
                     </div>
                 @endif
-                <div> 
+                <div class="flex flex-col items-center justify-center"> 
                     <div class="text-center p-6">
                         <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Üdvözöllek az eltűnt állatok oldalán!</h1>
                         <p class="mb-6 text-lg font-normal text-gray-700 lg:text-xl sm:px-16 xl:px-48 dark:text-slate-50">Ha eltűnt állatot találsz, vagy szeretnéd a saját állatodat megtalálni, jelentkezz be, vagy regisztrálj!</p>
 
                     </div>
-                    <div class="text-2xl font-bold text-center text-blue-600">
+                    
+                    <div class="text-2xl font-bold text-blue-600">
+                        <!--
                         <button class="bg-cyan-600 hover:bg-cyan-500 text-white font-bold py-2 px-4 rounded">Nézelődnék</button>
-                    </div>
+                        -->
+                    <button class="relative flex h-[50px] w-40 items-center justify-center overflow-hidden bg-gray-800 text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full  before:bg-cyan-500 before:duration-500 before:ease-out hover:shadow-cyan-500 hover:before:h-56 hover:before:w-56 rounded-sm">
+                        <span class="relative z-10">Nézelődnék</span>
+                      </button>
+                      </div>
+                </div>
+                <div class="fixed bottom-0 right-0 p-6 text-right z-1">
+                    <p class="font-semibold dark:text-white">website by: Alex Kosik & Mate Cserhati</p>
                 </div>
             </div>
-        </div>
+
+
+        
+        
+        
+
     </body>
 </html>
