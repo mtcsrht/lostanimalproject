ebe27ed999680f6bc3c8752ed086d83cf4d6f412
Mate <>
2024-03-13 09:07:12 +0100

    ui: flex-wrap added to my post container so now it shows the cards correctly

--- resources/views/myposts.blade.php
+++ resources/views/myposts.blade.php
@@ -33,31 +33,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="flex flex-col sm:flex-row justify-evenly item-start gap-5">
-                        @foreach ($animals as $animal)
-                            <div
-                                class="flex flex-col justify-between max-w-sm bg-white border border-gray-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5">
-                                <img class="w-[512px] rounded transition-all duration-300 filter grayscale hover:grayscale-0"
-                                    src="{{ asset("storage/$animal->image") }}" alt="" />
-                                <div class="p-5">
-                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                        {{ $animal->name }}</h5>
-                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                        {{ Str::of($animal->description)->limit(40) }}</p>
-                                    <a href="#"
-                                        class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                                        Edit
-                                        <svg class="ml-2 w-5 h-5  dark:text-gray-800 text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="2"
-                                                d="M7.75 4H19M7.75 4a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 4h2.25m13.5 6H19m-2.25 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 10h11.25m-4.5 6H19M7.75 16a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 16h2.25" />
-                                        </svg>
-                                    </a>
+                    <div class=" container">
+                        <div class="flex flex-col sm:flex-row sm:flex-wrap justify-evenly item-start gap-5">
+                            @foreach ($animals as $animal)
+                                <div
+                                    class="flex flex-col justify-between max-w-sm bg-white border border-gray-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5">
+                                    <img class="w-[512px] rounded transition-all duration-300 filter grayscale hover:grayscale-0"
+                                        src="{{ asset("storage/$animal->image") }}" alt="" />
+                                    <div class="p-5">
+                                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                            {{ $animal->name }}</h5>
+                                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                            {{ Str::of($animal->description)->limit(40) }}</p>
+                                        <a href="#"
+                                            class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                            Edit
+                                            <svg class="ml-2 w-5 h-5  dark:text-gray-800 text-white" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M7.75 4H19M7.75 4a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 4h2.25m13.5 6H19m-2.25 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 10h11.25m-4.5 6H19M7.75 16a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 16h2.25" />
+                                            </svg>
+                                        </a>
 
+                                    </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
