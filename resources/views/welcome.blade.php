359ee91b881acc27668955b042646b92d0ae3d0a
Máté Cserháti <mate.cserhati03@gmail.com>
2024-03-16 20:05:03 +0100

    Fonts are now from google api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,9 @@
         <title>{{env("APP_NAME")}}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
         <!-- Styles -->       
         @vite(['resources/css/app.css', 'resources/js/app.js'])     
