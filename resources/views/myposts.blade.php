c0856f63466ced57291f7d6a9cedcd6ebcfc5d6a
Máté Cserháti <mate.cserhati03@gmail.com>
2024-03-16 22:58:02 +0100

    ui:myposts currently won't redirect to anywhere (temporary)

--- resources/views/myposts.blade.php
+++ resources/views/myposts.blade.php
@@ -45,7 +45,7 @@
                                             {{ $animal->name }}</h5>
                                         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                                             {{ Str::of($animal->description)->limit(40) }}</p>
-                                        <a href="#"
+                                        <a href=""
                                             class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                             Edit
                                             <svg class="ml-2 w-5 h-5  dark:text-gray-800 text-white" aria-hidden="true"
