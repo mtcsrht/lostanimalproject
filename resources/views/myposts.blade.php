6524a9e8624865319efea40b65fab1e573ea2a0f
Mate <>
2024-03-19 08:33:26 +0100

    update: confirmation page to delete animal

--- resources/views/myposts.blade.php
+++ resources/views/myposts.blade.php
@@ -9,11 +9,14 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class=" container">
-                        <div class="flex flex-col sm:flex-row sm:flex-wrap justify-evenly item-start gap-5">
+                        <div class="flex item-center justify-center">
                             @if (session('status') === 'animal-deleted')
                                 <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
                                     class="text-sm text-gray-600 dark:text-gray-400">{{ __('Sikeres törlés!.') }}</p>
                             @endif
+                        </div>
+                        <div class="flex flex-col sm:flex-row sm:flex-wrap justify-evenly item-start gap-5">
+
                             @forelse ($animals as $animal)
                                 <div
                                     class="flex flex-col justify-between max-w-sm bg-white border border-gray-400 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5">
@@ -25,7 +28,7 @@
                                             {{ $animal->name }}</h5>
                                         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                                             {{ Str::of($animal->description)->limit(40) }}</p>
-                                        <p>{{ $animal->uuid}}</p>
+                                        <p>{{ $animal->uuid }}</p>
                                         <div class="flex gap-5">
                                             <a href=""
                                                 class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
@@ -38,13 +41,43 @@
                                                         d="M7.75 4H19M7.75 4a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 4h2.25m13.5 6H19m-2.25 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 10h11.25m-4.5 6H19M7.75 16a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 16h2.25" />
                                                 </svg>
                                             </a>
+                                            <!--
                                             <form action="{{ route('myposts.destroy', $animal) }}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-800 ...">
-                                                    Törlés 
+                                                    Törlés
                                                 </button>
                                             </form>
+                                        -->
+                                            <x-danger-button x-data=""
+                                                x-on:click.prevent="$dispatch('open-modal', 'confirm-animal-deletion')">{{ __('Állat törlése') }}</x-danger-button>
+
+                                            <x-modal name="confirm-animal-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+                                                <form method="post" action="{{ route('myposts.destroy', $animal) }}"
+                                                    class="p-6">
+                                                    @csrf
+                                                    @method('delete')
+
+                                                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                                        {{ __('Biztos szeretnéd törölni ezt az állatot?') }}
+                                                    </h2>
+
+                                                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                                        {{ __('Ha egyszer kitörlöd az állatot, később újra fel kell töltened!') }}
+                                                    </p>
+
+                                                    <div class="mt-6 flex justify-end">
+                                                        <x-secondary-button x-on:click="$dispatch('close')">
+                                                            {{ __('Mégse') }}
+                                                        </x-secondary-button>
+
+                                                        <x-danger-button class="ms-3">
+                                                            {{ __('Állat törlése') }}
+                                                        </x-danger-button>
+                                                    </div>
+                                                </form>
+                                            </x-modal>
                                         </div>
 
                                     </div>
@@ -56,6 +89,7 @@
                                 </div>
                             @endforelse
                         </div>
+
                     </div>
                 </div>
             </div>
