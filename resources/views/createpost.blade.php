ba46abfe453dddf123b56c0cf141de285db9480f
Máté Cserháti <mate.cserhati03@gmail.com>
2024-03-19 21:14:11 +0100

    ui: translated description

--- resources/views/createpost.blade.php
+++ resources/views/createpost.blade.php
@@ -63,8 +63,7 @@
                     <div class="mb-4">
 
                         <label for="description"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                            message</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Leírás</label>
                         <textarea id="description" name="description" rows="4"
                             class="block p-2.5 max-w-sm w-full h-44 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Írj az állatodról."></textarea>
