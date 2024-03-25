07fbd3905a09dc9491efaf7d2b0d48c0f6344030
mtcsrht <mate.cserhati03@gmail.com>
2024-03-25 13:54:10 +0100

    ui: back button that redirects you back where you came from

--- resources/views/layouts/public-navigation.blade.php
+++ resources/views/layouts/public-navigation.blade.php
@@ -2,23 +2,28 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16 w-full">
+            
             <div class="flex w-full justify-between">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ url('/')}}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
+                    <x-nav-link :href="url()->previous()" :active="request()->routeIs('login')" class="ml-5">
+                        {{ __('Back') }}
+                    </x-nav-link>
                 </div>
-
+                
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     @if (Route::has('login'))
-                    
+                        
                         @auth
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
                         @else
+                        
                         <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
                             {{ __('Login') }}
                         </x-nav-link>
