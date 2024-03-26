981f1cce67c57eeda21dd1a5c8e5a2323e15b766
mtcsrht <mate.cserhati03@gmail.com>
2024-03-26 12:44:40 +0100

    ui: dasboard's button is now removed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,9 +12,6 @@
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         {{ __("Üdvözöllek a menüben!") }}
                     </div>
-                    <a href="{{route('posts.show')}}" class="relative flex h-[50px] w-40 items-center justify-center overflow-hidden bg-gray-900 text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full  before:bg-cyan-500 before:duration-500 before:ease-out hover:shadow-cyan-500 hover:before:h-56 hover:before:w-56 rounded-sm">
-                        <span class="relative z-10">Nézelődnék</span>
-                    </a>
                 </div>
             </div>
         </div>
